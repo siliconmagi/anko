34c3aa82a473ad47c8270b85405744ff303cb5b3
siliconmagi <siliconmagi@yandex.com>
2017-08-03 19:16:58 -0700

    init

---
+++ app/src/main/java/com/nightshell/anko/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.nightshell.anko
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        helloButton.setOnClickListener{ toast("Hello Peeps")}
+    }
+}
